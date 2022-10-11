214037d3d3129467f8399cb7758a24b905f2c03d
arnvvd <pinot.arnaud@gmail.com>
2022-10-11 15:26:30 -0400

    Prepare slackbot.ts

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { APIGatewayEvent, Context } from 'aws-lambda'
-
-export async function handler (event: APIGatewayEvent, context: Context) {
-  return {
-    statusCode: 200,
-    body: "Hello, World"
-  };
-}
